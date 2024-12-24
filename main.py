1d01d67f5f5cdd40616928f2823e1afd8f1bcb07
jirofg <mechcromancer@gmail.com>
2024-12-23 18:58:03 -0600

    [FIX] Division de programa

--- main.py
+++ main.py
@@ -10,24 +10,49 @@ def main():
     try:
         topics = eval(os.getenv("TOPICS_PUBLICA"))
         while True:
-            print("A que topic desea publicar?")
-            for index, topic in enumerate(topics):
-                print(f"{index}. {topic}")
-            selected_topic = int(input())
-            print("Ingrese el mensaje que desea publicar:")
-            msg = input()
-            kafka_publisher = KafkaPublisher(topics)
             try:
-                kafka_publisher.publish_message(selected_topic, msg)
-                print("Mensaje publicado correctamente")
-            except KafkaException as exception:
-                print("Error al publicar el mensaje")
-            print("Desea publicar otro mensaje? Y/N")
-            repeat = input()
-            if repeat == "N" or repeat == "n":
-                raise KeyboardInterrupt
+                selected_topic = get_topic(topics)
+                msg = get_msg()
+                publish_msg(topics, selected_topic, msg)
+            except ValueError:
+                print("Por favor ingrese un numero valido")
+            repeat_menu()
     except KeyboardInterrupt as e:
         pass
+
+def get_topic(topics):
+    print("A que topic desea publicar?")
+    for index, topic in enumerate(topics):
+        print(f"{index}. {topic}")
+    selected_topic = int(input())
+    if selected_topic > len(topics)-1 or selected_topic < 0:
+        raise ValueError
+    return selected_topic
+
+def get_msg():
+    msg = ""
+    while msg == "":
+        print("Ingrese el mensaje que desea publicar:")
+        msg = input()
+    return msg
+
+def publish_msg(topics, selected_topic, msg):
+    kafka_publisher = KafkaPublisher(topics)
+    try:
+        kafka_publisher.publish_message(selected_topic, msg)
+        print("Mensaje publicado correctamente")
+    except KafkaException as exception:
+        print("Error al publicar el mensaje")
+
+def repeat_menu():
+    invalid = True
+    while invalid:
+        print("Desea publicar otro mensaje? Y/N")
+        repeat = input()
+        if repeat == "N" or repeat == "n":
+            raise KeyboardInterrupt
+        elif repeat == "Y" or repeat == "y":
+            invalid = False
 
 if __name__ == "__main__":
     main()
